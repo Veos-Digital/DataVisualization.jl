590ea5b271273dc9669396529a177109e86377d0
piever <pietro.vertechi@protonmail.com>
2021-09-27 15:34:35 +0200

    update gitignore

--- docs/make.jl
+++ docs/make.jl
@@ -1,1 +1,20 @@
 using Documenter, DataVisualization
+
+makedocs(;
+    modules=[DataVisualization],
+    authors="Pietro Vertechi <pietro.vertechi@veos.digital>",
+    repo="https://github.com/JuliaPlots/AlgebraOfGraphics.jl/blob/{commit}{path}#{line}",
+    sitename="Data Visualization",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+    ),
+    pages=Any[
+        "Home" => "index.md",
+    ],
+    strict=true,
+)
+
+deploydocs(;
+    repo="github.com/Veos-Digital/DataVisualization.jl",
+    push_preview=true,
+)
