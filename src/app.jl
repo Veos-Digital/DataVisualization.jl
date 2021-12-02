1a3357bcea55790e634f15f75c9b54a5584fa0c8
piever <pietro.vertechi@protonmail.com>
2021-12-02 16:16:57 +0100

    wip

--- src/app.jl
+++ src/app.jl
@@ -1,5 +1,5 @@
-const PipelineTabs = (; Load, Filter, Process)
-const VisualizationTabs = (; SpreadSheet, Chart)
+const PIPELINE_TABS = (; Load, Filter, Process)
+const VISUALIZATION_TABS = (; SpreadSheet, Chart)
 
 struct UI{P, V}
     pipelinetabs::P
@@ -14,7 +14,7 @@ end
 concatenate(names, value) = NamedTuple{names}(_concatenate(names, value))
 
 function _concatenate((p, ps...)::Tuple, value)
-    pipeline = PipelineTabs[p](value)
+    pipeline = PIPELINE_TABS[p](value)
     return (pipeline, _concatenate(ps, output(pipeline))...)
 end
 
@@ -30,22 +30,22 @@ Repetitions are allowed, for example setting
 `tabs=(:Load, :Filter, :Process, :Filter)` would generate a `UI` that
 allowes filtering both before and after processing the data.
 """
-function UI(table; pipelinetabs=keys(PipelineTabs), visualizationtabs=keys(VisualizationTabs))
+function UI(table; pipelinetabs=keys(PIPELINE_TABS), visualizationtabs=keys(VISUALIZATION_TABS))
     pipelines = concatenate(pipelinetabs, Observable(to_littledict(table)))
     value = output(last(pipelines))
-    visualizations = mapkeys(key -> VisualizationTabs[key](value), visualizationtabs)
+    visualizations = mapkeys(key -> VISUALIZATION_TABS[key](value), visualizationtabs)
     return UI(pipelines, visualizations)
 end
 
 function jsrender(session::Session, ui::UI)
     evaljs(session, js"document.body.classList.add('bg-gray-100');")
     pipelinetabs, visualizationtabs = Tabs(ui.pipelinetabs), Tabs(ui.visualizationtabs)
-    gui = DOM.div(
+    layout = DOM.div(
             class="grid grid-cols-3 h-full",
             DOM.div(class="col-span-1 pr-16", pipelinetabs),
             DOM.div(class="col-span-2 pl-16", visualizationtabs)
         )
-    return jsrender(session, gui)
+    return jsrender(session, layout)
 end
 
 """
@@ -54,7 +54,7 @@ end
 Generate a [`UI`](@ref) with a given `table` and list of `tabs`.
 Launch the output as a local app.
 """
-function app(table; pipelinetabs=keys(PipelineTabs), visualizationtabs=keys(VisualizationTabs))
+function app(table; pipelinetabs=keys(PIPELINE_TABS), visualizationtabs=keys(VISUALIZATION_TABS))
     return App() do
         return DOM.div(AllCSS..., UI(table; pipelinetabs, visualizationtabs))
     end
@@ -69,6 +69,6 @@ Serve the output at the given `url` and `port`.
 Return a `JSServe.Server` object.
 """
 function serve(table; url=Sockets.localhost, port=8081,
-               pipelinetabs=keys(PipelineTabs), visualizationtabs=keys(VisualizationTabs))
+               pipelinetabs=keys(PIPELINE_TABS), visualizationtabs=keys(VISUALIZATION_TABS))
     return Server(app(table; pipelinetabs, visualizationtabs), string(url), port)
 end
--- src/visualization/chart.jl
+++ src/visualization/chart.jl
@@ -1,3 +1,86 @@
+const PLOT_TYPES = (
+    scatter=Scatter,
+    lines=Lines,
+    barplot=BarPlot,
+    boxplot=BoxPlot,
+    violin=Violin,
+    heatmap=Heatmap,
+    contour=Contour,
+    contourf=Contourf,
+    wireframe=Wireframe,
+    surface=Surface,
+)
+
+const ANALYSES = (
+    histogram=AlgebraOfGraphics.histogram,
+    density=AlgebraOfGraphics.density,
+    frequency=AlgebraOfGraphics.frequency,
+    expectation=AlgebraOfGraphics.expectation,
+    linear=AlgebraOfGraphics.linear,
+    smooth=AlgebraOfGraphics.smooth,
+)
+
+const STYLES = (:color, :marker, :markersize, :linestyle, :col, :row, :layout, :side, :dodge, :stack)
+
+struct PlotSpecs
+    names::Observable{Vector{String}}
+    attributes::Observable{String}
+    layers::Observable{String}
+end
+
+function PlotSpecs(df::Observable)
+    return PlotSpecs(lift(colnames, df), Observable(""), Observable(""))
+end
+
+is_set(p::PlotSpecs) = !isempty(p.attributes[]) || !isempty(p.layers[])
+
+function to_visual(sym)
+    plt = get(PLOT_TYPES, sym, nothing)
+    isnothing(plt) || return visual(plt)
+    an = get(ANALYSES, sym, nothing)
+    isnothing(an) || return an()
+    msg = "plot or analysis not available"
+    throw(ArgumentError(msg))
+end
+
+to_symbolpair((a, b)) = Symbol(a) => Symbol(b)
+
+function to_algebraic(str::AbstractString)
+    calls = compute_calls(str)
+    layers = map(calls) do call
+        visual = mapreduce(to_visual∘Symbol, *, call.fs, init=mapping())
+        args = map(Symbol, call.positional)
+        attrs = map(to_symbolpair, call.named)
+        return visual * mapping(args...; attrs...)
+    end
+    return Layers(layers)
+end
+
+to_algebraic(v::Union{Tuple, AbstractArray}) = mapreduce(to_algebraic, *, v, init=mapping())
+
+to_algebraic(specs::PlotSpecs) = to_algebraic(map(getindex, (specs.attributes, specs.layers)))
+
+function layers_options()
+    options = vecmap(((keys(PLOT_TYPES)..., keys(ANALYSES)...))) do key
+        string(key) => String[]
+    end
+    pushfirst!(options, "+" => String[])
+    return options
+end
+
+function style_options(names)
+    options = vecmap(style -> string(style) => names, STYLES)
+    pushfirst!(options, "" => names)
+    return options
+end
+
+function specs_options(session::Session, specs::PlotSpecs; name)
+    return map(session, specs.names; result=Observable{AutocompleteOptions}()) do names
+        options = name == :layers ? [layers_options(); style_options(names)] : style_options(names)
+        return to_autocomplete_options(options)
+    end
+end
+
 struct Chart{T} <: AbstractVisualization{T}
     table::Observable{T}
     plotspecs::PlotSpecs
@@ -10,6 +93,17 @@ to_algebraic(chart::Chart) = data(chart.table[]) * to_algebraic(chart.plotspecs)
 defaultplot() = Figure(; backgroundcolor=colorant"#F3F4F6")
 
 function jsrender(session::Session, chart::Chart)
+
+    specs_widgets = map([:attributes, :layers]) do name
+        label = DOM.p(
+            class="text-blue-800 text-xl font-semibold p-4 w-full text-left",
+            uppercasefirst(string(name))
+        )
+        options = specs_options(session, chart.plotspecs; name)
+        textbox = jsrender(session, Autocomplete(getproperty(chart.plotspecs, name), options))
+        class = name == :layers ? "" : "mb-4"
+        return DOM.div(class=class, label, DOM.div(class="pl-4", textbox))
+    end
 
     plot = Observable{Figure}(defaultplot())
 
@@ -29,19 +123,14 @@ function jsrender(session::Session, chart::Chart)
     plot_button = Button("Plot", class=buttonclass(true))
     clear_button = Button("Clear", class=buttonclass(false))
     plotui = DOM.div(
-        chart.plotspecs,
-        DOM.div(class="mt-12 pl-4", plot_button, clear_button),
-        class="pr-16"
+        DOM.div(class="flex flex-row", specs_widgets),
+        DOM.div(class="mt-12 pl-4", plot_button, clear_button)
     )
 
     tryon(update_plot!, session, plot_button.value)
     tryon(update_plot!, session, chart.plotspecs.names) # gets updated when table changes
     tryon(reset_plot!, session, clear_button.value)
 
-    layout = DOM.div(
-        class="grid grid-cols-3",
-        DOM.div(class="col-span-1", plotui),
-        DOM.div(class="col-span-2 pl-16", plot)
-    )
+    layout = DOM.div(plotui, plot)
     return jsrender(session, layout)
 end
