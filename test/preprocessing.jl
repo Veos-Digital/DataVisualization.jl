635ceb68439e075116d98f9a8997e11529fc4dea
Mattia Bergomi <mattia.bergomi@veos.digital>
2021-12-08 10:18:07 +0100

    passing tests

--- test/preprocessing.jl
+++ test/preprocessing.jl
@@ -1,13 +1,26 @@
-using DataVisualization: logistic_scaler, binner
+using DataVisualization: log_scaler, logistic_scaler
+using DataVisualization: min_max_scaler, max_abs_scaler
+using DataVisualization: binner, quantile_binner, uniform_binner
 
-@testset "logistic scale test" begin
+@testset "Scaler test" begin
     isf = logistic_scaler([1.0])
     @test 15 |> isf |> inv(isf) ≈ 15
+    isf = log_scaler([1.0])
+    @test 15 |> isf |> inv(isf) ≈ 15
+    isf = min_max_scaler([1, 10])
+    @test 15 |> isf |> inv(isf) ≈ 15
+    isf = max_abs_scaler([1, 10])
+    @test 15 |> isf |> inv(isf) ≈ 15
+    
 end
 
-@testset "Binning" begin
-    column = [3, 2, 56, 4, 32, 4, 7, 88, 4, 3, 4]
+@testset "Binning test" begin
+    column = [0, 11, 23, 34, 50, 75, 98]
     bins = 0:20:100
-    result = [1, 1, 3, 1, 2, 1, 1, 5, 1, 1, 1]
+    result = [1, 1, 2, 2, 3, 4, 5]
     @test binner(column, bins) ≈ result
+    result = [1, 1, 2, 2, 3, 4, 6]
+    @test uniform_binner(column, 6) ≈ result
+    result = [1, 1, 2, 3, 4, 5, 6]
+    @test quantile_binner(column, 6) ≈ result
 end
